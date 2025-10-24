c7b92302878d56a81ed3a14b89bc413cd09955e1
sampahallen <sampahallen@gmail.com>
2025-10-24 23:17:54 +0000

    refactor layout and styles for Login and Signup pages for improved consistency and responsiveness

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,18 +4,18 @@ import flowers from "../assets/flowers.jpg";
 
 const SignupPage = () => {
   return (
-    <div className="min-h-screen flex flex-col-reverse lg:flex-row">
-      <div className="flex flex-col justify-center items-center w-full sm:w-1/2 px-10 sm:px-24">
+    <div className="lg:min-h-screen flex flex-col-reverse lg:flex-row">
+      <div className="flex flex-col items-center w-full lg:w-1/2 px-10 lg:px-24">
         <div className="w-full max-w-[430px]">
           <Signup />
         </div>
       </div>
 
-      <div className="flex justify-center items-center lg:w-1/2">
+      <div className="flex lg:w-1/2">
         <img
           src={flowers}
           alt="Floral artwork"
-          className="w-full h-[200px] md:h-[400px] max-sm:mb-3 lg:h-full object-cover lg:object-cover rounded-3xl py-4 px-5 sm:py-2 sm:px-2"
+          className="w-full h-[200px] md:h-[400px] max-sm:mb-3 lg:h-[800px] object-cover rounded-3xl py-2 px-2 sm:py-2 sm:px-2"
         />
       </div>
     </div>
